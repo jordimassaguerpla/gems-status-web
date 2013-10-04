1c6a47f6fe9f3d1552c388e48ab94f37d549509f
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2013-10-04 19:35:07 +0200

    fix: list security alerts belonging to the ruby application
    we were listing all security alerts belonging to the gem, but it should be
    the ones belonging to the gem and to the ruby application

--- app/models/ruby_application.rb
+++ app/models/ruby_application.rb
@@ -2,4 +2,5 @@ class RubyApplication < ActiveRecord::Base
   belongs_to :user
   has_many :ruby_application_ruby_gem_relationships
   has_many :ruby_gems, through: :ruby_application_ruby_gem_relationships
+  has_many :security_alerts
 end
