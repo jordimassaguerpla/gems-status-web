8bf80fe7085e8dac5a8d12f7e75d51dc4b81659a
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2013-10-07 12:05:18 +0200

    write tests for the edit and update actions for the sec. alert controller

--- test/controllers/security_alerts_controller_test.rb
+++ test/controllers/security_alerts_controller_test.rb
@@ -9,4 +9,14 @@ class SecurityAlertsControllerTest < ActionController::TestCase
     get :show, id: @security_alert
     assert_response :success
   end
+
+  test "should get edit" do
+    get :edit, id: @security_alert
+    assert_response :success
+  end
+
+  test "should update security alert" do
+    patch :update, id: @security_alert, security_alert: { comment: @security_alert.comment, status: @security_alert.status }
+    assert_redirected_to security_alert_path(assigns(:security_alert))
+  end
 end
