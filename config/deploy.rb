f235b500b4753cf13d674ba58a26ba0d4161dc43
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2013-11-06 14:13:47 +0100

    add symlink to database configuration when deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :deploy_to, '/srv/www/vhosts/gems-status-web'
 # set :log_level, :debug
 # set :pty, true
 
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :linked_dirs, %w{tmp log vendor/bundle}
 
