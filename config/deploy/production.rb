a316d69675a5ac2269646979f49ebd16c0115e9b
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2013-11-08 13:35:23 +0100

    set production environment like staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,14 @@
-set :stage, :production
+set :stage, :staging
+set :rails_env, :production
 
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deploy@redruby.io}
+role :web, %w{deploy@redruby.io}
+role :db,  %w{deploy@redruby.io}
 
 # Extended Server Syntax
 # ======================
@@ -15,7 +16,8 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a has can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app db}, my_property: :my_value
+server 'redruby.io', user: 'deploy', roles: %w{web app db}
+#, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
@@ -39,4 +41,4 @@ server 'example.com', user: 'deploy', roles: %w{web app db}, my_property: :my_va
 #   }
 # setting per server overrides global ssh_options
 
-# fetch(:default_env).merge!(rails_env: :production)
+# fetch(:default_env).merge!(rails_env: :staging)
