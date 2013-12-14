4d3a8efd86446d87754dd79661fccd537b968447
Jordi Massaguer Pla <jordimassaguerpla@gmail.com>
2013-12-14 17:47:45 +0100

    add beta user management for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -79,6 +79,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin, :role)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin, :role, :beta_user)
     end
 end
