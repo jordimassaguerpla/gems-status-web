537816cbe3deba176991ef026ffee1b098be39bd
Jordi Massaguer Pla <jordimassaguerpla@gmail.com>
2013-12-18 17:43:43 +0100

    add tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+
+  setup do
+    @user = users(:two)
+    session[:user_id] = User.find_by_name("two")
+  end
+
+  test "index" do
+    get 'index'
+    assert_response :success
+  end
+
   test "ping" do
+    session[:user_id] = nil
     get 'ping'
     assert_response :success
   end
--- test/models/ruby_application_test.rb
+++ test/models/ruby_application_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class RubyApplicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get gems with security alerts" do
+    @ruby_application = ruby_applications(:one)
+    sa = @ruby_application.gems_with_security_alerts
+    assert_equal 0, sa.length
+    @ruby_application = ruby_applications(:two)
+    sa = @ruby_application.gems_with_security_alerts
+    assert_equal 1, sa.length
+    assert_equal security_alerts(:two).ruby_gem.name, sa.first
+  end
 end
