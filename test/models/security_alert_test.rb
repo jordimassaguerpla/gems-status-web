9f15672631410b0e2654eba4bf1d334870036612
Jordi Massaguer Pla <jordimassaguerpla@gmail.com>
2014-01-24 16:20:38 +0100

    refactor: extract query for similar alerts to model from views
    this is the first step to implement issue#15

--- test/models/security_alert_test.rb
+++ test/models/security_alert_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class SecurityAlertTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "similars" do
+    sa1 = security_alerts(:one)
+    sa1.desc ="A"
+    sa1.save
+    sa2 = security_alerts(:two)
+    sa2.desc = "B"
+    sa2.save
+    result = sa1.similars
+    assert(!result.include?(sa2))
+    sa2.desc = "A"
+    sa2.save
+    result = sa1.similars
+    assert(result.include?(sa2))
+  end
 end
